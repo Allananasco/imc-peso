0c133c77ea89e4cf058e655f7f291177328c814c
ALANA DO NASCIMENTO <00001098869126sp@aluno.educacao.sp.gov.br>
2024-06-04 13:54:17 -0300

    atualizado htmll 3.0

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("teste")
+function calcular(){
+    var fPeso = parseFloat(document.getElementById("Peso"))
+    var fAltura = parseFloat(document.getElementById("Altura"))
+    var fIMC = fPeso/(fAltura*fAltura)
+     
+}
